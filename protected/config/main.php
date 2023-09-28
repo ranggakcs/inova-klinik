b20f27dc8b3da5010556c14416b74d99e54101e1
ranggakcs <ranggakusumah94@gmail.com>
2023-09-28 08:09:17 +0700

    Menambahkan file

---
+++ protected/config/main.php
@@ -0,0 +1,94 @@
+<?php
+
+// uncomment the following to define a path alias
+Yii::setPathOfAlias('local','protected/extensions/bootstrap-theme');
+
+// This is the main Web application configuration. Any writable
+// CWebApplication properties can be configured here.
+return array(
+	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+	'name'=>'Sistem Informasi klinik Inova',
+	'theme'=>'bootstrap',
+
+	// preloading 'log' component
+	'preload'=>array('log'),
+
+	// autoloading model and component classes
+	'import'=>array(
+		'application.models.*',
+		'application.components.*',
+
+		'ext.bootstrap-theme.widgets.*',
+		'ext.bootstrap-theme.helpers.*',
+		'ext.bootstrap-theme.behaviors.*',
+	),
+
+	'modules'=>array(
+		// uncomment the following to enable the Gii tool
+		
+		'gii'=>array(
+			'class'=>'system.gii.GiiModule',
+			'password'=>'inovapass',
+			// If removed, Gii defaults to localhost only. Edit carefully to taste.
+			'ipFilters'=>array('127.0.0.1','::1'),
+
+			'generatorPaths'=>array(
+				'ext.bootstrap-theme.gii',
+			),
+		),
+		
+	),
+
+	// application components
+	'components'=>array(
+
+		'user'=>array(
+			// enable cookie-based authentication
+			'allowAutoLogin'=>true,
+		),
+
+		// uncomment the following to enable URLs in path-format
+		/*
+		'urlManager'=>array(
+			'urlFormat'=>'path',
+			'rules'=>array(
+				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
+				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
+				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
+			),
+		),
+		*/
+
+		// database settings are configured in database.php
+		'db'=>require(dirname(__FILE__).'/database.php'),
+
+		'errorHandler'=>array(
+			// use 'site/error' action to display errors
+			'errorAction'=>YII_DEBUG ? null : 'site/error',
+		),
+
+		'log'=>array(
+			'class'=>'CLogRouter',
+			'routes'=>array(
+				array(
+					'class'=>'CFileLogRoute',
+					'levels'=>'error, warning',
+				),
+				// uncomment the following to show log messages on web pages
+				/*
+				array(
+					'class'=>'CWebLogRoute',
+				),
+				*/
+			),
+		),
+
+	),
+
+	// application-level parameters that can be accessed
+	// using Yii::app()->params['paramName']
+	'params'=>array(
+		// this is used in contact page
+		'adminEmail'=>'webmaster@example.com',
+	),
+);
